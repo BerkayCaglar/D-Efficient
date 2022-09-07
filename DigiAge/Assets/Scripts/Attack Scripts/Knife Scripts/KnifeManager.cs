7e5f3304e51946074202f52818159480ce035ccc
Efendi3 <98265489+Efendi3@users.noreply.github.com>
2022-09-07 17:03:31 +0300

    Updated
    Added knife

---
+++ DigiAge/Assets/Scripts/Attack Scripts/Knife Scripts/KnifeManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeManager : MonoBehaviour
+{
+    [SerializeField]private GameObject knife;
+    private int knifeCount = 3;
+    private bool isCoolDown;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1) && knifeCount > 0)
+        {
+            Destroy(Instantiate(knife, transform.position + new Vector3(0f,0.8f,0f), transform.rotation),5);
+            knifeCount--;
+        }
+        else if (knifeCount == 0 && !isCoolDown)
+        {
+            isCoolDown = true;
+            StartCoroutine(KnifeCoolDown());
+        }
+    }
+
+    private IEnumerator KnifeCoolDown()
+    {
+        yield return new WaitForSeconds(5);
+        knifeCount = 3;
+        isCoolDown = false;
+    }
+}
---
+++ DigiAge/Assets/Scripts/Attack Scripts/Knife Scripts/KnifeMovementManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeMovementManager : MonoBehaviour
+{
+    private float axeRotateSpeed = 2000f,axeParentMovementSpeed = 30f;
+    public bool knifeHitsAnObject = false;
+    void Update()
+    {
+        // Knife'ı döndürüyoruz
+        //transform.Rotate(Vector3.down * Time.deltaTime * axeRotateSpeed);
+        
+        // Knife'ı hareket ettiriyoruz.
+        if (!knifeHitsAnObject)
+        {
+            transform.parent.transform.Translate((Vector3.forward * Time.deltaTime *axeParentMovementSpeed));
+        }
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag(("Wall")) || other.gameObject.CompareTag(("Ground")))
+        {
+            knifeHitsAnObject = true;
+        }
+    }
+}
